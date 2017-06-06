e0de25942e0e4ecbed52e3cef00e2e062f952c65
aray <aray.kalimbetova@mail.ru>
2017-06-06 17:04:54 +0600

    home work-lesson13

---
+++ script13.js
@@ -0,0 +1,17 @@
+ var counter = 0;
+  function count() {
+   counter++;
+  }
+  
+  function myfunc() {
+   alert(counter);
+  }
+  
+  count();
+  count();
+  count();
+  count();
+  count();
+  
+  myfunc();
+  
